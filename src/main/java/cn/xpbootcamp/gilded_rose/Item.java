9b05de2422ccc23cdf44c912e6a2596f1e9e1626
Yuan Shenjian <sjyuan@thoughtworks.com>
2020-05-18 11:39:21 +0800

    Init
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

---
+++ src/main/java/cn/xpbootcamp/gilded_rose/Item.java
@@ -0,0 +1,21 @@
+package cn.xpbootcamp.gilded_rose;
+
+public class Item {
+
+    public String name;
+
+    public int sell_in;
+
+    public int quality;
+
+    public Item(String name, int sell_in, int quality) {
+        this.name = name;
+        this.sell_in = sell_in;
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", " + this.sell_in + ", " + this.quality;
+    }
+}
