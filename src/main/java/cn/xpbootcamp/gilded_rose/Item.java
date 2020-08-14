4833e8cded1780e7db2f8404d16dd25dbea5f668
QingQing Dou <qqdou@thoughtworks.com>
2020-08-14 22:59:49 +0800

    refactor: extract methods of equals logic
    Signed-off-by: QingQing Dou <qqdou@thoughtworks.com>

--- src/main/java/cn/xpbootcamp/gilded_rose/Item.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Item.java
@@ -18,4 +18,66 @@ public class Item {
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    public void update() {
+        if (!isAgedBrie() && !isBackstagePass()) {
+            if (quality > 0) {
+                if (!isSulfuras()) {
+                    quality = quality - 1;
+                }
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (isBackstagePass()) {
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!isSulfuras()) {
+            sellIn = sellIn - 1;
+        }
+
+        if (sellIn < 0) {
+            if (!isAgedBrie()) {
+                if (!isBackstagePass()) {
+                    if (quality > 0) {
+                        if (!isSulfuras()) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = 0;
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+    }
+
+    private boolean isSulfuras() {
+        return name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isBackstagePass() {
+        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isAgedBrie() {
+        return name.equals("Aged Brie");
+    }
 }
